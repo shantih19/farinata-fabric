5e0b203f1ba1d40e0a37603c18771197a2082aa8
shantih19 <shantih19@gmail.com>
2023-06-16 01:18:10 +0200

    first commit

---
+++ src/main/kotlin/com/shantih19/farinata/ExampleMod.kt
@@ -0,0 +1,15 @@
+package com.shantih19.farinata
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object ExampleMod : ModInitializer {
+    private val logger = LoggerFactory.getLogger("farinata")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
---
+++ src/main/kotlin/com/shantih19/farinata/ExampleModDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.shantih19.farinata
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object ExampleModDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
